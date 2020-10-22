bd586d589d8e8f6947ae3ed2a77c69812df3ef56
SHIRA BEN ZAKEN <67286268+ShiraBenZaken@users.noreply.github.com>
2020-10-22 17:00:55 +0300

    Create date.js

---
+++ backend/date.js
@@ -0,0 +1,19 @@
+export default {
+    products:[{
+        _id:'123',
+        name: 'ארמון',
+        category:'שמן',
+        image: "./images/house.jpg",
+        price:'1000$',
+        brand:'תמונה מהממת'
+    },
+    {
+        _id:'111',
+        name: 'בית קטן',
+        category:'אקריליק',
+        image: './images/house.jpg',
+        price:'1500$',
+        brand:'משהו משהו'
+    }
+]
+}
