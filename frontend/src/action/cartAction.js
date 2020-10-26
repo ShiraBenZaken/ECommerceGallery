dbd08f2c9d29a2f541832c1c06955d1a7d3f9acb
SHIRA BEN ZAKEN <67286268+ShiraBenZaken@users.noreply.github.com>
2020-10-26 14:13:00 +0200

    Update cartAction.js

--- frontend/src/action/cartAction.js
+++ frontend/src/action/cartAction.js
@@ -5,6 +5,13 @@ const addToCart=(productId)=> async(dispatch)=> {
 
     try{
         const {data} = await Axios.get("/api/products/" + productId);
+        dispatch({type:CART_ADD_ITEM, payload:{
+            product:data._id,
+            name:data.name,
+            image:data.image,
+            price:data.price,
+            countInStock:data.countInStock,
+        }})
     }
     catch (error)
     {
